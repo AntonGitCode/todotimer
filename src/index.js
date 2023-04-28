0ce6139031c2688d6486af7514654b28fc14b1c6
Anton Chernetsov <anton.chernetsov@gmail.com>
2023-04-29 02:58:00 +0300

    fixed branch

--- src/index.js
+++ src/index.js
@@ -5,8 +5,4 @@ import './index.css'
 import App from './components/App'
 
 const root = ReactDOM.createRoot(document.getElementById('wrapper'))
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+root.render(<App />)
