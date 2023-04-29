52bf085a2f0842dd09d8a73073a57c0050b4ff2d
Anton Chernetsov <anton.chernetsov@gmail.com>
2023-04-29 11:04:04 +0300

    last v before create new branch

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './components/TodoApp'
 
-const root = ReactDOM.createRoot(document.getElementById('wrapper'))
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
